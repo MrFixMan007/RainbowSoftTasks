92b60eddd019cd4e9332dd1272e5b33f4bde2d4f
MrFixMan007 <125643669+MrFixMan007@users.noreply.github.com>
2023-06-30 12:03:04 +0300

    принимает и обрабатывает параметры
    генерит ответы по параметрам, отправляет ответ по веб-сокету, но на странице пока не выводит

--- ex6WebSocket/main.go
+++ ex6WebSocket/main.go
@@ -3,7 +3,11 @@ package main
 import (
 	"fmt"
 	"html/template"
+	"math/rand"
 	"net/http"
+	"strconv"
+	"sync"
+	"time"
 
 	"github.com/gorilla/websocket"
 )
@@ -14,30 +18,93 @@ var upgrader = websocket.Upgrader{
 }
 
 func serveWs(w http.ResponseWriter, r *http.Request) {
-	fmt.Println("serveWs запустился")
-	defer fmt.Println("serveWs закрылся")
-	fmt.Println(w)
-	fmt.Println(r)
-
-	ws, err := upgrader.Upgrade(w, r, nil)
+	connection, err := upgrader.Upgrade(w, r, nil)
 	if err != nil {
-		if _, ok := err.(websocket.HandshakeError); !ok {
-			fmt.Println(err)
-		}
 		fmt.Println(err)
 		return
 	}
-	ws.WriteMessage(websocket.PingMessage, []byte{13, 21, 32, 13, 213})
-	fmt.Println("serveWs написал")
+	channel := make(chan int)
+	wg := &sync.WaitGroup{}
+	fmt.Println(r.URL.Query().Get("flow"))
+	fmt.Println(r.URL.Query().Get("limit"))
+	fmt.Println(r.URL.Query().Get("count"))
+
+	flow, _ := strconv.Atoi(r.URL.Query().Get("flow"))
+	limit, _ := strconv.Atoi(r.URL.Query().Get("limit"))
+	count, _ := strconv.Atoi(r.URL.Query().Get("count"))
+
+	if flow <= 0 || limit <= 0 || count <= 0 {
+		connection.WriteMessage(websocket.TextMessage, []byte("Входные данные должны быть > 0"))
+	} else {
+		wg.Add(1)
+		go Printer(flow, limit, count, wg, channel)
+		for v := range channel {
+			connection.WriteMessage(websocket.TextMessage, []byte{byte(v)})
+			fmt.Println("отправил", v)
+		}
+		wg.Wait()
+	}
+
+	// for {
+	// 	_, message, _ := connection.ReadMessage()
+
+	// 	connection.WriteMessage(websocket.TextMessage, message)
+	// 	go PrintMessage(string(message))
+	// }
+}
+
+// Printer создаёт горутины, передаёт им параметры. Передаст в канал столько
+// чисел, сколько требуется.
+func Printer(flow int, limit int, count int, wg *sync.WaitGroup, out chan int) {
+	defer fmt.Println("Printer завершил работу -")
+	defer wg.Done()
+	fmt.Println("Printer начал работу +")
+	channel := make(chan int)
+	quit := make(chan bool)
+	wg1 := &sync.WaitGroup{}
+
+	for i := 0; i < flow; i++ {
+		wg1.Add(1)
+		go Generator(limit, channel, quit, wg1, i)
+	}
+
+	for i := 0; i < int(count); i++ {
+		quit <- true
+		out <- <-channel
+	}
+
+	close(quit)
+	wg1.Wait()
+	close(channel)
+	close(out)
+}
+
+// Generator создаёт случайные числа от 0 до лимита и передаёт в канал
+func Generator(limit int, channel chan int, quit chan bool, wg1 *sync.WaitGroup, index int) {
+	defer fmt.Printf("Generator %d завершился -\n", index+1)
+	defer wg1.Done()
+	fmt.Printf("Generator %d запустился +\n", index+1)
+
+	for {
+		_, ok := <-quit
+		if ok {
+			rand.Seed(time.Now().UnixNano())
+			channel <- rand.Intn(int(limit))
+		} else {
+			break
+		}
+	}
+}
+
+func PrintMessage(message string) {
+	fmt.Println(message)
 }
 
 func serveHome(w http.ResponseWriter, r *http.Request) {
 	var v = struct {
 		Host string
-		Data string
 	}{
 		r.Host,
-		"Hi!!!!!",
 	}
 	templ, _ := template.ParseFiles("index.html")
 	templ.Execute(w, &v)
