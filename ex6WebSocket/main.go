5b3187cc8bedae1f465604a62a71c7c70fe9cd8c
MrFixMan007 <125643669+MrFixMan007@users.noreply.github.com>
2023-07-19 23:01:16 +0400

    it works

--- ex6WebSocket/main.go
+++ ex6WebSocket/main.go
@@ -7,50 +7,15 @@ import (
 	"net/http"
 	"strconv"
 	"sync"
-	"time"
 
 	"github.com/gorilla/websocket"
 )
 
+var flow, limit, count string
+
 var upgrader = websocket.Upgrader{
 	ReadBufferSize:  1024,
 	WriteBufferSize: 1024,
-}
-
-func serveWs(w http.ResponseWriter, r *http.Request) {
-	connection, err := upgrader.Upgrade(w, r, nil)
-	if err != nil {
-		fmt.Println(err)
-		return
-	}
-	channel := make(chan int)
-	wg := &sync.WaitGroup{}
-	fmt.Println(r.URL.Query().Get("flow"))
-	fmt.Println(r.URL.Query().Get("limit"))
-	fmt.Println(r.URL.Query().Get("count"))
-
-	flow, _ := strconv.Atoi(r.URL.Query().Get("flow"))
-	limit, _ := strconv.Atoi(r.URL.Query().Get("limit"))
-	count, _ := strconv.Atoi(r.URL.Query().Get("count"))
-
-	if flow <= 0 || limit <= 0 || count <= 0 {
-		connection.WriteMessage(websocket.TextMessage, []byte("Входные данные должны быть > 0"))
-	} else {
-		wg.Add(1)
-		go Printer(flow, limit, count, wg, channel)
-		for v := range channel {
-			connection.WriteMessage(websocket.TextMessage, []byte{byte(v)})
-			fmt.Println("отправил", v)
-		}
-		wg.Wait()
-	}
-
-	// for {
-	// 	_, message, _ := connection.ReadMessage()
-
-	// 	connection.WriteMessage(websocket.TextMessage, message)
-	// 	go PrintMessage(string(message))
-	// }
 }
 
 // Printer создаёт горутины, передаёт им параметры. Передаст в канал столько
@@ -59,6 +24,7 @@ func Printer(flow int, limit int, count int, wg *sync.WaitGroup, out chan int) {
 	defer fmt.Println("Printer завершил работу -")
 	defer wg.Done()
 	fmt.Println("Printer начал работу +")
+
 	channel := make(chan int)
 	quit := make(chan bool)
 	wg1 := &sync.WaitGroup{}
@@ -88,7 +54,6 @@ func Generator(limit int, channel chan int, quit chan bool, wg1 *sync.WaitGroup,
 	for {
 		_, ok := <-quit
 		if ok {
-			rand.Seed(time.Now().UnixNano())
 			channel <- rand.Intn(int(limit))
 		} else {
 			break
@@ -96,11 +61,8 @@ func Generator(limit int, channel chan int, quit chan bool, wg1 *sync.WaitGroup,
 	}
 }
 
-func PrintMessage(message string) {
-	fmt.Println(message)
-}
-
-func serveHome(w http.ResponseWriter, r *http.Request) {
+// HomeHandler отправляет заголовок и вызывает открытие главной страницы
+func HomeHandler(w http.ResponseWriter, r *http.Request) {
 	var v = struct {
 		Host string
 	}{
@@ -110,9 +72,50 @@ func serveHome(w http.ResponseWriter, r *http.Request) {
 	templ.Execute(w, &v)
 }
 
+// WsHandler вызывает апгрейд до веб-сокета, запускает генерацию ответов
+func WsHandler(w http.ResponseWriter, r *http.Request) {
+	connection, err := upgrader.Upgrade(w, r, nil)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	channel := make(chan int)
+	wg := &sync.WaitGroup{}
+
+	flow, _ := strconv.Atoi(flow)
+	limit, _ := strconv.Atoi(limit)
+	count, _ := strconv.Atoi(count)
+
+	if flow <= 0 || limit <= 0 || count <= 0 {
+		connection.WriteMessage(websocket.TextMessage, []byte("Входные данные должны быть > 0"))
+	} else {
+		wg.Add(1)
+		go Printer(flow, limit, count, wg, channel)
+		for v := range channel {
+			err := connection.WriteMessage(websocket.TextMessage, []byte(fmt.Sprintf("%d", v)))
+			//time.Sleep(5 * time.Millisecond)
+			if err != nil {
+				fmt.Println(err)
+				return
+			}
+			fmt.Println("отправил", v)
+
+		}
+		wg.Wait()
+	}
+}
+
+// PostHandler принимает параметры
+func PostHandler(w http.ResponseWriter, r *http.Request) {
+	flow = r.URL.Query().Get("flow")
+	limit = r.URL.Query().Get("limit")
+	count = r.URL.Query().Get("count")
+}
+
 func main() {
-	http.HandleFunc("/", serveHome)
-	http.HandleFunc("/ws", serveWs)
+	http.HandleFunc("/", HomeHandler)
+	http.HandleFunc("/post", PostHandler)
+	http.HandleFunc("/ws", WsHandler)
 	if err := http.ListenAndServe(":8080", nil); err != nil {
 		fmt.Println(err)
 	}
