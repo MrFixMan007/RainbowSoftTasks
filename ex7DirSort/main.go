e802722f13c3313f036cf4d96011104a71ad9ab0
danilaStager <schurkin.danil@yandex.ru>
2023-07-24 17:04:37 +0300

    done on function wih structure

--- ex7DirSort/main.go
+++ ex7DirSort/main.go
@@ -11,12 +11,14 @@ import (
 	"strings"
 )
 
+type File struct {
+	Type string
+	Name string
+	Size int64
+}
+
 func main() {
-	type File struct {
-		Type string
-		Name string
-		Size int64
-	}
+
 	//Флаги
 	root, sortType, err := flagParsing()
 	if err != nil {
@@ -24,16 +26,12 @@ func main() {
 		os.Exit(1)
 	}
 
-	types, names, values, err := listDirByReadDir(root)
+	files, err := listDirByReadDir(root)
 	if err != nil {
 		fmt.Println(err)
 		os.Exit(2)
 	}
 
-	var files = []File{}
-	for i, v := range values {
-		files = append(files, File{types[i], names[i], v})
-	}
 	//сортировка
 	if strings.EqualFold(sortType, "asc") {
 		fmt.Println("Сортировка в порядке возрастания")
@@ -75,43 +73,35 @@ func GetSize(root string) (int64, error) {
 }
 
 // listDirByReadDir возвращает типы, названия подпапок, файлов и их размеры по переданному адресу
-func listDirByReadDir(path string) ([]string, []string, []int64, error) {
-	var types []string
-	var names []string
-	var sizes []int64
+func listDirByReadDir(path string) ([]File, error) {
+	var files []File
 	lst, err := ioutil.ReadDir(path)
 	if err != nil {
-		return types, names, sizes, fmt.Errorf("ошибка с чтением директории: %s", err)
+		return files, fmt.Errorf("ошибка с чтением директории: %s", err)
 	}
 	for _, val := range lst {
 		if val.IsDir() { // Обработка подпапки и её внутренностей
-			types1, names1, sizes1, err := listDirByReadDir(fmt.Sprintf("%s/%s", path, val.Name())) //Рекурсивный вызов функции
+			files1, err := listDirByReadDir(fmt.Sprintf("%s/%s", path, val.Name())) //Рекурсивный вызов функции
 			if err != nil {
 				fmt.Printf("ошибка с чтением директории: %s", err)
 				continue
 			}
 			value := 0
-			for _, v := range sizes1 { //Подсчитываем размер папки
-				value += int(v)
+			for i := range files1 { //Подсчитываем размер папки
+				value += int(files1[i].Size)
 			}
 
-			types = append(types, "dir")
-			names = append(names, fmt.Sprintf("%s/%s", path, val.Name())) //Добавляем подпапку
-			sizes = append(sizes, int64(value))                           //Добавляем размер подпапки
+			files = append(files, File{"type", fmt.Sprintf("%s/%s", path, val.Name()), int64(value)})
 
-			types = append(types, types1...)
-			names = append(names, names1...) //Добавляем подфайлы
-			sizes = append(sizes, sizes1...) //Добавляем размеры подфайлов
+			files = append(files, files1...)
 		} else { // Обработка файла
 			value, err := GetSize(fmt.Sprintf("%s/%s", path, val.Name()))
 			if err != nil {
 				fmt.Printf("ошибка при чтении размера файла: %s", err)
 				continue
 			}
-			types = append(types, "file")
-			sizes = append(sizes, value)                                  //Добавляем файлы
-			names = append(names, fmt.Sprintf("%s/%s", path, val.Name())) //Добавляем размеры файлов
+			files = append(files, File{"file", fmt.Sprintf("%s/%s", path, val.Name()), value})
 		}
 	}
-	return types, names, sizes, nil
+	return files, nil
 }
